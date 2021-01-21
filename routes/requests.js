88f9defd1f59bf7e0e3c9c90c0278e42989d3150
IVAN <ivanbatur5g@gmail.com>
2021-01-21 14:38:24 +0100

    Wrote route for declining friend request, need to test

--- routes/requests.js
+++ routes/requests.js
@@ -40,7 +40,7 @@ router.post("/:userId", async(req, res, next) => {
         res.status(400).json({success:false, msg: e.message})
     }
 })
-
+///ACCEPT FRIEND REQUEST
 router.post("/:userId/accept", async (req,res) => {
     
     try {
@@ -57,6 +57,26 @@ router.post("/:userId/accept", async (req,res) => {
         req.user.friends.push(req.params.userId)
         const updatedUser = await User.findByIdAndUpdate(req.user._id, req.user, {new: true})
         if(!updatedUser) throw Error("Something went wrong with saving the user")
+        res.status(200).json({success: true, updatedUser})
+    }
+    catch(e) {
+        res.status(400).json({success:false, msg: e.message})
+    }
+})
+//DECLINE FRIEND REQUEST
+router.post("/:userId/decline", async (req, res) => {
+    try {
+        //FIND IF THERE IS A FRIEND REQUEST IN THE USERS F.R. ARRAy
+        const index = req.user.friendRequests.findIndex((id) => String(id) === String(req.params.userId))
+        if (index === -1 ) throw Error("Friend request doesnt exist")
+        //CHECK IF ALREDY FRIENDS
+        const frIndx = req.user.friends.findIndex((id) => String(id) === String(req.params.userId)) 
+        if (frIndx !== -1) throw Error("Already friends with this user")
+        //REMOVE FRIEND REQUEST
+        req.user.friendRequests = req.user.friendRequests.filter((id) => String(id) !== String(req.params.userId))
+
+        const updatedUser = await User.findByIdAndUpdate(req.user._id, req.user, {new: true})
+        if (!updatedUser) throw("Something went wrong with saving the user")
         res.status(200).json({success: true, updatedUser})
     }
     catch(e) {
