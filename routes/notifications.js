16cbb6dbe124fdde42ceb1663c58b4e8f0d1c18d
IVAN <ivanbatur5g@gmail.com>
2021-01-22 20:42:28 +0100

    Added route for marking notifications read

--- routes/notifications.js
+++ routes/notifications.js
@@ -10,6 +10,7 @@ router.use(passport.authenticate('jwt', { session: false }))
 router.get("/", async(req, res) => {
     try {
         const notifications = await Notification.find({recipient: req.user._id})
+        if(!notifications) throw Error("No notifications found")
         res.status(200).json({success: true, notifications})
     }
     catch(e) {
@@ -17,4 +18,14 @@ router.get("/", async(req, res) => {
     }
 })
 
+router.put("/", async (req, res) => {
+    try {
+        const response = await Notification.updateMany({recipient: req.user._id, seen: false}, {seen: true})
+        if(!response) throw Error("Something went wrong with updating notifications")
+        res.status(200).json({success: true, response})
+    }
+    catch(e){
+        res.status(400).json({success: false, msg: e.message})
+    }
+})
 module.exports = router;
