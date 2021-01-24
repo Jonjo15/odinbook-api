13f27b9d4d1b03749197d0039bfd32c01d88c11e
IVAN <ivanbatur5g@gmail.com>
2021-01-24 13:09:48 +0100

    Improved get home route

--- routes/index.js
+++ routes/index.js
@@ -13,19 +13,20 @@ router.get('/', function(req, res, next) {
 
 router.get("/home",passport.authenticate('jwt', { session: false }), async(req, res) => {
   //TODO:
-  const friends = req.user.friends;
+  let friends = req.user.friends;
+  friends.push(req.user._id)
   let timelinePosts = [];
   try {
-    const yourPosts = await Post.find({creator: req.user._id})
-    .populate("creator", "first_name family_name _id")
-    .populate({
-      path: 'comments',
-      populate: [{
-       path: 'creator',
-       select: 'first_name family_name _id'
-      //  model: 'Component'
-      }] 
-    }).limit(4).sort({ "createdAt": -1 })
+    // const yourPosts = await Post.find({creator: req.user._id})
+    // .populate("creator", "first_name family_name _id")
+    // .populate({
+    //   path: 'comments',
+    //   populate: [{
+    //    path: 'creator',
+    //    select: 'first_name family_name _id'
+    //   //  model: 'Component'
+    //   }] 
+    // }).limit(4).sort({ "createdAt": -1 })
     //TODO: FINISH THIS QUERY
     const friendPosts = await Post.find({creator: {$in: friends}})
     .populate("creator", "first_name family_name _id")
@@ -36,8 +37,8 @@ router.get("/home",passport.authenticate('jwt', { session: false }), async(req, 
        select: 'first_name family_name _id'
       //  model: 'Component'
       }]
-    }).limit(21).sort({"createdAt": -1})
-    timelinePosts = [...yourPosts, ...friendPosts]
+    }).limit(25).sort({"createdAt": -1})
+    timelinePosts = [ ...friendPosts]
     res.status(200).json({success: true, timelinePosts})
   }
   catch(e) {
