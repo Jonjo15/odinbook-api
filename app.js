0d09cea21e455f5bc5f6aae55e33b3b37daee32d
IVAN <ivanbatur5g@gmail.com>
2021-01-27 15:49:53 +0100

    Added cors

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ require("dotenv").config()
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const authRouter = require("./routes/auth")
+const cors = require("cors")
 const requestRouter = require("./routes/requests")
 const notificationRouter = require("./routes/notifications")
 const passport = require("passport")
@@ -37,7 +38,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(cors())
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use("/requests", requestRouter)
